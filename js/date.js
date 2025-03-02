ed9aad0613882fdafda1da93f060fed1bbb49c76
hasinahsanul <hasinahsanul@gmail.com>
2025-03-02 20:38:30 +0600

    current date show on the top card

---
+++ js/date.js
@@ -0,0 +1,16 @@
+// Function to show the current date
+function formatDate(date) {
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
+
+const currentDate = new Date();
+
+const formattedDate = formatDate(currentDate);
+
+document.getElementById("currentDate").textContent = formattedDate;
